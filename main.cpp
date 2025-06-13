cbd896c348f63155968bd7ea27e4f5978491a84c
sano <s2210729@gl.cc.uec.ac.jp>
2025-06-13 11:27:30 +0900

    Add function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 
+int function(int x){
+	return x*x;
+}
+
 int main(void){
 	
 	std::cout << "git" << std::endl;
 	std::cout << "GIT" << std::endl;
 
-        for(int i=0; i<10; i++){
-          std::cout << i << std::endl;
-        }
+		for(int i=0; i<10; i++){
+	 		std::cout << i << std::endl;
+		}
+	
+	std::cout << function(3) << std::endl;
 
 	return 0;
 
